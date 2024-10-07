188a40f8af05eb23daffec0a7249026bc92cf7e4
Rohith Jayawardene <gambol99@gmail.com>
2024-10-07 17:21:51 +0100

    chore: we need to ensure we are using the tenant, not the central provider

--- data.tf
+++ data.tf
@@ -1,11 +1,16 @@
 
 ## Get the current account for the current account 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  provider = aws.tenant
+}
 
 ## Get the current region for the current account 
-data "aws_region" "current" {}
+data "aws_region" "current" {
+  provider = aws.tenant
+}
 
 ## Get a list of availablility zones for the current region 
 data "aws_availability_zones" "current" {
-  state = "available"
+  state    = "available"
+  provider = aws.tenant
 }
